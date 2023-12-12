08d32cf16ac8f49747f1c5c465f2062606acb11d
gacmen45 <marekgacek45@gmail.com>
2023-12-12 06:47:49 +0100

    create route.js file

---
+++ src/router.js
@@ -0,0 +1,66 @@
+
+import { createRouter, createWebHistory } from 'vue-router';
+
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+      { path: '/', redirect: '/teams' },
+      // { path: '/teams', component: TeamsList, alias: '/' },
+      {
+        name: 'teams',
+        path: '/teams',
+        meta:{needsAuth:true},
+        components: {
+          default: TeamsList,
+          footer: TeamsFooter,
+        },
+        beforeEnter(to,from,next){
+          console.log('beforeEnter');
+          console.log(to,from);
+          next()
+        },
+        children: [
+          {
+            name: 'team-members',
+            path: ':teamId',
+            component: TeamMembers,
+            props: true,
+          },
+        ],
+      },
+      { path: '/users', components: { default: UsersList, footer: UsersFooter } },
+      { path: '/:notFound(.*)', redirect: '/teams' },
+    ],
+    linkActiveClass: 'active',
+    scrollBehavior(to, from, savedPosition) {
+      // console.log(to, from, savedPosition);
+      if (savedPosition) {
+        return savedPosition;
+      }
+      return {
+        left: 0,
+        top: 0,
+      };
+    },
+  });
+  
+  router.beforeEach((to, from, next) => {
+    console.log('before each');
+    console.log(to,from);
+    if(to.meta.needsAuth){
+      console.log('needs auth');
+      next();
+    }else{
+  next()
+    }
+  });
+
+
+  export default router
